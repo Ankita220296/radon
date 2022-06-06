2684fa5d0a0b3f3646b8ab1835121ca79477e134
Ankita220296 <sanganiankita223@gmail.com>
2022-06-07 01:37:07 +0530

    Assignment/mongoDb-part2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const UserController = require("../controllers/userController");
+const BookController = require("../controllers/bookController");
 
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+  res.send("My first ever api!");
+});
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBook", BookController.createBook);
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getBooksData", BookController.getBooksData);
 
-router.post("/createBook", BookController.createBook  )
+router.get("/getXINRBooks", BookController.getXINRBooks);
 
-router.get("/getBooksData", BookController.getBooksData)
+router.post("/getBooksInYear", BookController.getBooksInYear);
+
+router.post("/getParticularBooks", BookController.getParticularBooks);
+
+router.get("/getRandomBooks", BookController.getRandomBooks);
 
 module.exports = router;
