dd62d474a5c299aa5340114baae61024743f9062
Ankita220296 <sanganiankita223@gmail.com>
2022-06-14 21:07:37 +0530

    Assignment/Auth-1

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Ankita220296:Ankita704696@cluster0.d9vvv.mongodb.net/UserAuthJWT-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
